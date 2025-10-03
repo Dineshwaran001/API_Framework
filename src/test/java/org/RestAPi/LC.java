e641fa5a6b146c7c3fd2dc60c9dce9755728a36e
dineshwaran.t <warandinesh33@gmail.com>
2025-10-03 10:28:11 +0530

    TestNG

---
+++ src/test/java/org/RestAPi/LC.java
@@ -0,0 +1,4 @@
+package org.RestAPi;
+
+public class LC {
+}
---
+++ src/test/java/org/RestAPi/sample/IntegrationSample.java
@@ -0,0 +1,41 @@
+package org.RestAPi.sample;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class IntegrationSample {
+    @Test
+    @Owner("Dineshwaran")
+    @Description("This TC is to verify the Booking 1")
+    public void TokenGeneration(){
+        System.out.println("This is a sample TC");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    @Owner("Dineshwaran")
+    @Description("This TC is to verify the Booking 1")
+    public void BookingCreation(){
+        System.out.println("This is a sample TC");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    @Owner("Dineshwaran")
+    @Description("This TC is to verify the Booking 1")
+    public void UpdateBooking(){
+        System.out.println("This is a sample TC");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    @Owner("Dineshwaran")
+    @Description("This TC is to verify the Booking 1")
+    public void DeleteBooking(){
+        System.out.println("This is a sample TC");
+        Assert.assertTrue(true);
+    }
+
+}
